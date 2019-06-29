c221e33c154fcb9af1ab49c03af51dd06095304c
Wandalen <wandalen.me@gmail.com>
2019-06-29 23:00:09 +0300

    auto commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -9,26 +9,16 @@ var b = { name : 'b', nodes : [] } // 2
 var c = { name : 'c', nodes : [] } // 3
 var d = { name : 'd', nodes : [] } // 4
 var e = { name : 'e', nodes : [] } // 5
-var f = { name : 'f', nodes : [] } // 6
-var g = { name : 'g', nodes : [] } // 7
-var h = { name : 'h', nodes : [] } // 8
-var i = { name : 'i', nodes : [] } // 9
-var j = { name : 'j', nodes : [] } // 10
 
 a.nodes.push( b ); // 1
-b.nodes.push( e, f ); // 2
+b.nodes.push( e, d ); // 2
 c.nodes.push( b ); // 3
-d.nodes.push( a, g ); // 4
-e.nodes.push( a, c, h ); // 5
-f.nodes.push(); // 6
-g.nodes.push( h ); // 7
-h.nodes.push( i ); // 8
-i.nodes.push( f, h ); // 9
-j.nodes.push(); // 10
+d.nodes.push( a, e ); // 4
+e.nodes.push( a, c, d ); // 5
 
 var sys = new _.graph.AbstractGraphSystem({ onNodeNameGet : ( node ) => node.name });
 var group = sys.groupMake();
-group.nodesAdd([ a, b, c, d, e, f, g, h, i, j ]);
+group.nodesAdd([ a, b, c, d, e ]);
 
 console.log( 'Tree' );
 console.log( group.nodesInfoExportAsTree([ a ]) );
@@ -40,9 +30,8 @@ Tree
    +-- b
      +-- e
      | +-- c
-     | +-- h
-     |   +-- i
-     |     +-- f
-     +-- f
-
+     | +-- d
+     +-- d
+       +-- e
+         +-- c
 */
