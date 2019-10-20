824e1e709b600af2ab58c2911f68f65b3fe1eeef
Wandalen <wandalen.me@gmail.com>
2019-10-20 05:27:41 +0300

    experiment

---
+++ proto/dwtools/amid/l1/graphTools/IncludeLive.s
@@ -0,0 +1,37 @@
+( function _IncludeLive_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  // if( typeof _global_ === 'undefined' || !_global_.wBase )
+  // {
+  //   let toolsPath = '../../../dwtools/Base.s';
+  //   let toolsExternal = 0;
+  //   try
+  //   {
+  //     toolsPath = require.resolve( toolsPath );
+  //   }
+  //   catch( err )
+  //   {
+  //     toolsExternal = 1;
+  //     require( 'wTools' );
+  //   }
+  //   if( !toolsExternal )
+  //   require( toolsPath );
+  // }
+  // var _ = _global_.wTools;
+
+  let _ = require( '../../../Tools.s' );
+
+  require( './IncludeBase.s' );
+
+  require( './l5_live/LiveNode.s' );
+  require( './l5_live/LiveNodeIn.s' );
+  require( './l5_live/LiveNodeOut.s' );
+  require( './l5_live/LiveSystem.s' );
+
+}
+
+})();
