dd0ef48280dc49f2eaa1f2a5cd13554ea77aa421
Jack Shell <jackshel45@gmail.com>
2021-02-01 14:43:44 +0200

    Fixed to pass tests

--- proto/wtools/amid/l1/graphTools/IncludeTop.s
+++ proto/wtools/amid/l1/graphTools/IncludeTop.s
@@ -12,8 +12,6 @@ if( typeof module !== 'undefined' )
 
   // require( './UseAbstractMid.s' );
 
-  require( './IncludeTop.s' )
-
   require( './l3/GraphBranch.s' );
   require( './l3/GraphNode.s' );
   require( './l3/GraphSystem.s' );
